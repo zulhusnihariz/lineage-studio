db0dba3af0901f5d2e664a3b236ebd35b9554393
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-09-05 23:43:55 +0800

    add rpc.slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,16 @@
-import { create } from 'zustand';
-import { ModalSlice, createModalSlice } from './slices/modal.slice';
-import { WalletSlice, createWalletSlice } from './slices/wallet.slice';
+import { create } from 'zustand'
+import { ModalSlice, createModalSlice } from './slices/modal.slice'
+import { WalletSlice, createWalletSlice } from './slices/wallet.slice'
+import { RPCSlice, createRPCSlice } from './slices/rpc.slice'
 
-type ResetAllSlices = { resetAllSlices: () => void };
-type BoundStoreType = ModalSlice & ResetAllSlices & WalletSlice;
+type ResetAllSlices = { resetAllSlices: () => void }
+type BoundStoreType = ModalSlice & ResetAllSlices & WalletSlice & RPCSlice
 
-export const resetters: (() => void)[] = [];
+export const resetters: (() => void)[] = []
 
 export const useBoundStore = create<BoundStoreType>()((...a) => ({
   ...createModalSlice(...a),
   ...createWalletSlice(...a),
+  ...createRPCSlice(...a),
   resetAllSlices: () => resetters.forEach(resetter => resetter()),
-}));
+}))
