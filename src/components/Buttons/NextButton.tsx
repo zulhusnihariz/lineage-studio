6126a09cfdfe1794ad3d25685fa324b5eb32334d
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-09-07 03:52:52 +0800

    update previous button arrow

--- src/components/Buttons/NextButton.tsx
+++ src/components/Buttons/NextButton.tsx
@@ -16,6 +16,18 @@ function NextActiveIcon() {
   )
 }
 
+function PreviousActiveIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 mr-2">
+      <path
+        fill-rule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-4.28 9.22a.75.75 0 000 1.06l3 3a.75.75 0 101.06-1.06l-1.72-1.72h5.69a.75.75 0 000-1.5h-5.69l1.72-1.72a.75.75 0 00-1.06-1.06l-3 3z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  )
+}
+
 const NextButton = (props: ButtonProp) => {
   return (
     <button
@@ -25,7 +37,7 @@ const NextButton = (props: ButtonProp) => {
       <span className="absolute inset-0 translate-x-0.5 translate-y-0.5 bg-indigo-600 transition-transform group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
       <span className="flex items-center relative border border-current bg-white px-8 py-3">
-        <NextActiveIcon />
+        {props.name === 'Next' ? <NextActiveIcon /> : <PreviousActiveIcon />}
         {props.name}
       </span>
     </button>
